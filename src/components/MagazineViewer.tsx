1ce6c4767b0af23353c175fcd2760827349b848c
坂井 歩夢 <sakai.ayumu@databroad.net>
2024-06-13 14:15:37 +0900

    update

--- src/components/MagazineViewer.tsx
+++ src/components/MagazineViewer.tsx
@@ -6,10 +6,20 @@ import "swiper/scss/pagination";
 import "./MagazineViewer.scss";
 
 const slides: string[] = [
-    "http://192.168.11.32/page01.png",
-    "http://192.168.11.32/page02.png",
-    "http://192.168.11.32/page03.png",
-    "http://192.168.11.32/page04.png",
+    " https://http.cat/100.png",
+    " https://http.cat/101.png",
+    " https://http.cat/102.png",
+    " https://http.cat/103.png",
+    " https://http.cat/200.png",
+    " https://http.cat/201.png",
+    " https://http.cat/202.png",
+    " https://http.cat/203.png",
+    " https://http.cat/204.png",
+    " https://http.cat/205.png",
+    " https://http.cat/206.png",
+    " https://http.cat/207.png",
+    " https://http.cat/208.png",
+    " https://http.cat/404.png",
 ];
 
 export default function MagazineViewer() {
